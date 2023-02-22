701539729fcda5959cab10623d8313ff6804d30b
erosMariano <erosmariano1@gmail.com>
2023-02-22 18:14:38 -0300

    feat: contato, footer, habilidades, hero, mobile

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -2,39 +2,47 @@ const divToObserve = document.querySelector("#habilidades");
 const card = document.querySelectorAll(".habilidades__cards .card");
 const descriptions = document.querySelectorAll("#habilidades .description");
 
-const observer = new IntersectionObserver((entries, observer) => {
-  const [entry] = entries;
+if (window.innerWidth > 1046) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    const [entry] = entries;
 
-  if (entry.isIntersecting) {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY < entry.target.clientHeight) {
-        removeActive();
-        card[0].classList.add("active");
-        descriptions[0].classList.add("active");
-      } else if (window.scrollY < entry.target.clientHeight + 300) {
-        removeActive();
-        card[1].classList.add("active");
-        descriptions[1].classList.add("active");
-      } else if (window.scrollY < entry.target.clientHeight + 500) {
-        removeActive();
-        card[2].classList.add("active");
-        descriptions[2].classList.add("active");
+    if (entry.isIntersecting) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY < entry.target.clientHeight) {
+          removeActive();
+          card[0].classList.add("active");
+          descriptions[0].classList.add("active");
+        } else if (window.scrollY < entry.target.clientHeight + 300) {
+          removeActive();
+          card[1].classList.add("active");
+          descriptions[1].classList.add("active");
+        } else if (window.scrollY < entry.target.clientHeight + 500) {
+          removeActive();
+          card[2].classList.add("active");
+          descriptions[2].classList.add("active");
+        }
+      });
+    }
+  });
+
+  function removeActive() {
+    card.forEach((el) => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
+      }
+    });
+
+    descriptions.forEach((el) => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
       }
     });
   }
-});
-
-function removeActive() {
-  card.forEach((el) => {
-    if (el.classList.contains("active")) {
-      el.classList.remove("active");
-    }
-  });
-
-  descriptions.forEach((el) => {
-    if (el.classList.contains("active")) {
-      el.classList.remove("active");
-    }
-  });
+  observer.observe(divToObserve);
 }
-observer.observe(divToObserve);
+
+// if (window.innerWidth <= 840) {
+//   document
+//     .querySelector("#contato .contato__container div > span")
+//     .insertAdjacentElement("afterend", document.querySelector(".form"));
+// }
