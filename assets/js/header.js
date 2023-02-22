eb71724cf279932065465020b1389df481625f2b
erosMariano <erosmariano1@gmail.com>
2023-02-22 17:07:53 -0300

    Feat: responsive hero

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,22 @@
-const header = document.querySelector(".header")
-window.addEventListener("scroll", (e) =>{
-
-  if(window.scrollY > 100){
-    header.classList.add("active")
-  }else{
-    if(header.classList.contains("active")){
-      header.classList.remove("active")
+const header = document.querySelector(".header");
+if (window.innerWidth > 768) {
+  window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 100) {
+      header.classList.add("active");
+    } else {
+      if (header.classList.contains("active")) {
+        header.classList.remove("active");
+      }
     }
-  }
-})
+  });
+} else {
+  window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 50) {
+      header.classList.add("active");
+    } else {
+      if (header.classList.contains("active")) {
+        header.classList.remove("active");
+      }
+    }
+  });
+}
